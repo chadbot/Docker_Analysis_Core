9c476bb5282dd68771e6e3d5763c9c993b1931a3
Chad Borkenhagen <chad.borkenhagen@gmail.com>
2022-05-04 19:00:11 +0000

    Added VSCode devcontainer files

---
+++ .devcontainer/scripts/install_dev_tools.sh
@@ -0,0 +1,40 @@
+# packages and config files specific to develoment in a VS Code container.
+
+## Configure R
+
+# install packages required for VSCode-R (use renv to copy to cache)
+R --quiet --no-init-file -e "renv::install(c('jsonlite', 'rlang'))"
+R --quiet --no-init-file -e "renv::install(c('languageserver', 'httpgd'))"
+
+# add VSCode-R config options to .RProfile
+echo -e '
+
+source(file.path(Sys.getenv("HOME"), ".vscode-R", "init.R"))
+options(vsc.use_httpgd = TRUE)
+
+' >> ~/.Rprofile
+
+# add radian console
+sudo /usr/bin/pip3 install radian
+
+
+## Add development tools to Conda base environment
+
+# install conda/python dev tools
+conda env update --name base -f docker_meta/.devcontainer/config/base_dev.yml
+
+
+## Other useful settings
+
+# add mouse scrolling in tmux 
+echo '
+set -g mouse on
+' > ~/.tmux.conf
+
+# add mouse scrolling in screen
+echo '
+# change escape key
+escape ^Bb
+# enable scrolling
+termcapinfo xterm* ti@:te@
+' > ~/.screenrc
